a23f6310d18f6ee9dc5144c2ed6f061162b69c8a
Diego <drescu@gmail.com>
2020-08-30 14:56:20 -0300

    24-registro de categorias

---
+++ vistas/scripts/categoria.js
@@ -0,0 +1,89 @@
+var tabla;
+
+// Funcion que se ejecuta al inicio 
+function init() {
+    mostrarform(false);
+    listar();
+
+    $("#formulario").on("submit",function(e) {
+        guardaryeditar(e);
+    })
+}
+
+// Funcion limpiar 
+function limpiar() {
+    // el '#nombre' se refiere al objeto del formulario html por su id.
+    // funcion val, devolvera un valor vacio. 
+    $("#idcategoria").val("");
+    $("#nombre").val("");  
+    $("#descripcion").val("");
+}
+
+// Funcion mostrar formulario
+function mostrarform(flag) {
+    limpiar();
+    if(flag) {
+        $("#listadoregistros").hide();
+        $("#formularioregistros").show();
+        $("#btnGuardar").prop("disabled",false);
+    } else {
+        $("#listadoregistros").show();
+        $("#formularioregistros").hide();
+    }
+}
+
+// Funcion cancelar form
+function cancelarform() {
+    limpiar();
+    mostrarform(false);
+}
+
+// Funcion listar
+function listar() { 
+    tabla=$('#tbllistado').dataTable(
+        {
+        "aProcessing": true,    // Activamos el procesamiento del datatables
+        "aServerSide": true,    // Paginacion y filtrado realizados por el servidor
+        dom: 'Bfrtip',          // Definimos los elementos del control de tabla
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdf'
+        ],
+        "ajax": {
+            url: '../ajax/categoria.php?op=listar',
+            type: "get",
+            dataType: "json",
+            error: function(e) {
+                console.log(e.responseText);
+            }
+        },
+        "bDestroy": true,
+        "iDisplayLength": 5,            // paginacion
+        "order": [[ 0, "desc" ]]        // orden de datos: columna, orden
+    }).DataTable(); 
+}
+
+function guardaryeditar(e) {
+    e.preventDefault();     // no se activara la accion predeterminada del evento 
+    $("#btnGuardar").prop("disabled",true);
+    var formData = new FormData($("#formulario")[0]);
+
+    $.ajax({
+        url: "../ajax/categoria.php?op=guardaryeditar",
+        type: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        
+        success: function(datos) {
+            alert(datos);
+            mostrarform(false);
+            tabla.ajax.reload();
+        } 
+    });
+    limpiar();
+}
+
+init();
